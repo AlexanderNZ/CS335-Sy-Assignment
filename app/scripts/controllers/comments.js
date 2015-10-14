1c222bc0f17f00ff0ce66fab02b4c87a1491f45f
Alex Corkin <alex.corkin@sparkventures.co.nz>
2015-10-15 10:28:28 +1300

    add stub js files

---
+++ app/scripts/controllers/comments.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name securityApp.controller:CommentsCtrl
+ * @description
+ * # CommentsCtrl
+ * Controller of the securityApp
+ */
+angular.module('securityApp')
+  .controller('CommentsCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/shop.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name securityApp.controller:ShopCtrl
+ * @description
+ * # ShopCtrl
+ * Controller of the securityApp
+ */
+angular.module('securityApp')
+  .controller('ShopCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
